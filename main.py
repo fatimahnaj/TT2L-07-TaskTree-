9e97b2676db128aa6959f31c7947971f8ac72c4c
Fatimah Najihah <fatimahnajihah05@gmail.com>
2024-04-20 13:13:44 +0800

    feat: testing switching screens

--- main.py
+++ main.py
@@ -5,14 +5,35 @@ screen_size = (1540,800)
 screen = pygame.display.set_mode(screen_size)
 pygame.display.set_caption('TaskTree')
 
-run = True
-while run:
+
+def screen_startup():
+    run = True
+    while run:
 
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
 
-    pygame.display.flip() #updates the display
+        screen.fill((200,200,200))
+        if pygame.mouse.get_pressed()[0] == 1:
+            screen_home()
+        pygame.display.flip()
 
-pygame.quit()
+    pygame.quit()
+
+def screen_home():
+    run = True
+    while run:
+
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+        screen.fill((150, 75, 0))
+        pygame.display.flip()
+        
+    pygame.quit()
+
+screen_startup()
