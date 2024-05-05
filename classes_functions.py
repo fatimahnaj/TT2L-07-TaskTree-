e8cba5d73eaa4dd3c0e37657d47078431bc11c50
Anisah <anisahasan2905@gmail.com>
2024-05-05 16:29:54 +0800

    'delete-screen'

--- classes_functions.py
+++ classes_functions.py
@@ -77,8 +77,7 @@ class Button():
 
     def checkforinput(self, position):
         if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
-            return True
-        return False
+            print("Button Press!")
     
     def changecolor(self, position):
         if position [0]in range(self.rect.left, self.rect.right) and position [1] in range(self.rect.top, self.rect.bottom):
