9f4956a390e15ebfabcaaa6882b03308db061078
Anisah <anisahasan2905@gmail.com>
2024-05-18 22:41:04 +0800

    'delete-anisah-button'

--- classes_functions.py
+++ classes_functions.py
@@ -83,38 +83,6 @@ def convert_time(time,x,y,size):
     display_time = TEXT(f"{display_minutes:02}",x,y,size,black)
     display_time.display_text()
 
-#anisah punya button
-class Button():
-    def __init__(self, image, pos, text_input, text_size, base_color, hovering_color):
-        self.image = image
-        self.x_pos = pos[0]
-        self.y_pos = pos[1]
-        self.font = pygame.font.Font("I-pixel-u.ttf", text_size)
-        self.base_color, self.hovering_color = base_color, hovering_color
-        self.text_input = text_input
-        self.text = self.font.render(self.text_input, True, self.base_color)
-        if self.image is None :
-            self.image = self.text
-        else:
-            self.image = pygame.transform.smoothscale(self.image, (width, height))
-        self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
-        self.text_rect = self.text.get_rect(center=(self.x_pos, self.y_pos))
-
-    def update(self, screen=screen):
-        if self.image is not None :
-            screen.blit(self.image, self.rect)
-        screen.blit(self.text, self.text_rect)
-
-    def checkforinput(self, position):
-        if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
-            print("Button Press!")
-    
-    def changecolor(self, position):
-        if position [0]in range(self.rect.left, self.rect.right) and position [1] in range(self.rect.top, self.rect.bottom):
-            self.text = self.font.render(self.text_input, True, self.hovering_color)
-        else:
-            self.text = self.font.render(self.text_input, True, self.base_color)
-
 #Ummu Level Bar
 class LevelBar():
     def __init__(self, x, y, w, h, level):
